3c7ee7b3fbca6f7d866c37cf4dcbba9ea1506fcb
Gilad Weiss <33327017+weissgilad@users.noreply.github.com>
2020-03-13 11:59:40 +0200

    update dev (#11)

---
+++ echo_filter.py
@@ -0,0 +1,47 @@
+from util.punctuation_cleaner import cleanPunctuation
+
+# TODO split into files and clean up this file
+
+
+def having(bot, update):
+    msg = update.effective_message.text
+    words = cleanPunctuation(msg).split()
+    idx = 0
+
+    for w in words:
+        if w == u"יש" or w == u"אין":
+            break
+        else:
+            idx = idx + 1
+
+    if idx == len(words):
+        return False
+
+    words = words[idx:]
+
+    for i, _ in enumerate(words):
+        if words[i] == u"אין":
+            words[i] = u"יש"
+        elif words[i] == u"יש":
+            words[i] = u"אין"
+        elif words[i] == u"לי":
+            words[i] = u"לך"
+        elif words[i] == u"לך":
+            words[i] = u"לי"
+
+    update.effective_message.reply_text(' '.join(words))
+    return True
+
+
+filter_list = [having]
+
+
+def default_action(bot, update):
+    update.effective_message.reply_text(update.effective_message.text + 'v2')
+
+
+def echo_filter(bot, update):
+    for filter in filter_list:
+        if filter(bot, update):
+            return
+    default_action(bot, update)
