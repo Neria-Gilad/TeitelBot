a969e1000da7aef3287b4f0098760625cf3be3ac
Neria laptop <neriat@gmail.com>
2021-01-30 21:10:30 +0200

    add config class for watson translator

--- teitelbot/config/__init__.py
+++ teitelbot/config/__init__.py
@@ -10,3 +10,11 @@ TOKEN = environ.get('TOKEN')
 PORT = int(environ.get('PORT', 3000))
 IS_RUN_REMOTE = string_utils.is_true(environ.get('IS_RUN_REMOTE', 'false'))
 CHANCE_OF_RANDOM_RESPONSE = float(environ.get('CHANCE_OF_RANDOM_RESPONSE', 1.0))
+
+
+class Watson:
+    class Translator:
+        API_KEY = environ.get('WATSON_TRANSLATOR_API_KEY')
+        SERVICE_URL = environ.get('WATSON_TRANSLATOR_SERVICE_URL',
+                                  'https://api.eu-gb.language-translator.watson.cloud.ibm.com')
+        VERSION = environ.get('WATSON_TRANSLATOR_VERSION', '2018-05-01')
